bd00e52b49da3bba12ca73d2294859c761ffcc1a
sr2498 <sr2498@njit.edu>
2024-07-03 22:43:10 -0400

    added many files and modified them

---
+++ todo-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AddToDo from './components/AddToDo'
+
+const App = () => {
+  return (
+    <main>
+      <h1>ToDo-List APP</h1>
+      <AddToDo />
+    </main>
+  )
+}
+
+export default App
---
+++ todo-app/src/components/AddToDo.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+const AddToDo = () => {
+    const [todo, setTodo] = useState("");
+  return (
+    <form >
+        <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} />
+        <button type='submit'>Add</button>       
+    </form>
+  )
+}
+
+export default AddToDo
