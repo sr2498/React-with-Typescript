d8ecc463c1e750cd5045c4e1c919482715b6ba88
Sandhya <134464080+sr2498@users.noreply.github.com>
2025-03-26 09:50:13 -0400

    Update main.tsx

--- todo-app/src/main.tsx
+++ todo-app/src/main.tsx
@@ -5,7 +5,6 @@ import './index.css'
 import { TodosProvider } from './store/todos.tsx'
 import { BrowserRouter } from 'react-router-dom'
 
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
