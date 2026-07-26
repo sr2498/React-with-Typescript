0b55b395c4c618896b0e3c8436426838553b74b2
Sandhya <134464080+sr2498@users.noreply.github.com>
2026-07-26 14:24:33 -0400

    Remove unnecessary blank lines in main.tsx

--- todo-app/src/main.tsx
+++ todo-app/src/main.tsx
@@ -5,7 +5,6 @@ import './index.css'
 import { TodosProvider } from './store/todos.tsx'
 import { BrowserRouter } from 'react-router-dom'
 
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
@@ -15,6 +14,3 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </BrowserRouter>
   </React.StrictMode>,
 )
-
-
-
